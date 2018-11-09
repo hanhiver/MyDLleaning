22ed463a82bd858e0a284313a853c45fb3f4f12d
Dong Han <handongfr@gmail.com>
2018-11-09 15:03:13 +0800

    Update MyCode/perceptronTest.py

--- MyCode/perceptronTest.py
+++ MyCode/perceptronTest.py
@@ -2,18 +2,24 @@
 
 import matplotlib.pyplot as plt 
 
+TRAIN_ITERATION = 100
+TRAIN_RATE = 0.01
+PLOT_XLIM = 50
+
 class Perceptron:
     def __init__(self, input_para_num, acti_func):
         # 初始化激活函数
         self.activator = acti_func
 
         # 把所有参数W初始化都设置为0
-        # self.weights = [1.0 for _ in range(input_para_num)]
-        self.weights = [1.3, 2.2, 3.1]
+        #self.weights = [1.0 for _ in range(input_para_num)]
+        self.weights = [1.1, 1.2, 1.1]
 
+        # 记录参数W1, W2, b和模型训练损失历史。
         self.W1_hist = []
         self.W2_hist = []
         self.b_hist = []
+        self.loss_hist = []
 
     def __str__(self):
         # 打印出所有的参数，按照W1, W2, b的顺序
@@ -45,6 +51,9 @@ class Perceptron:
         # 根据预测的输出值调整参数。
         # 计算损失值
         delta = input_vec_label[-1] - prediction
+
+        # 将当前的损失记录到历史中。
+        self.loss_hist.append(delta)
         
         # 根据损失值调整参数，学习率为rate。
         for i in range(len(self.weights)):
@@ -64,6 +73,10 @@ def get_training_dataset():
     # dataset for OR
     #dataset = [[-1, 1, 1, 1], [-1, 0, 0, 0], [-1, 1, 0, 1], [-1, 0, 1, 1]]
 
+    # dataset for XOR
+    # 请注意，选择这个数据会导致模型无法收敛最终训练失败。
+    # dataset = [[-1, 1, 1, 0], [-1, 0, 0, 0], [-1, 1, 0, 1], [-1, 0, 1, 1]]
+
     return dataset
 
 def train_and_perceptron():
@@ -74,7 +87,7 @@ def train_and_perceptron():
     dataset = get_training_dataset()
 
     # 进行1000次训练，训练的学习率是0.01
-    p.train(dataset, 120, 0.01)
+    p.train(dataset, TRAIN_ITERATION, TRAIN_RATE)
 
     return p
 
@@ -91,18 +104,32 @@ if __name__ == '__main__':
     print ('input [1, 0] = output %d' % perception.predict([-1, 1, 0]))
     print ('input [1, 1] = output %d' % perception.predict([-1, 1, 1]))
 
+    # 如果你的运行环境报告没有安装matplotlib，可以通过"pip install matplotlib"命令安装
+    # 或者简单的屏蔽第一行代码和以下所有代码。
     # 打印出W1, W2, b跟随训练次数收敛图像。
     plt.figure()
     x = range(len(perception.W1_hist))
-    #plt.xlim(0, 30)
-    plt.plot(
-        x, perception.W1_hist, 
-        x, perception.W2_hist,
-        x, perception.b_hist)
-    plt.legend(('W1', 'W2', 'bias'), 
-        bbox_to_anchor = (1.1, 1.05), 
-        loc = 'upper right', 
-        borderaxespad = 0.1)
+    
+    # 打印参数W1趋势
+    plt.subplot(2, 2, 1, title = "Param W1")
+    plt.xlim(0, PLOT_XLIM)
+    plt.plot(x, perception.W1_hist)
+    
+    # 打印参数W2趋势
+    plt.subplot(2, 2, 2, title = "Param W2")
+    plt.xlim(0, PLOT_XLIM)
+    plt.plot(x, perception.W2_hist)
+    
+    # 打印偏置b趋势
+    plt.subplot(2, 2, 3, title = "Bias b")
+    plt.xlim(0, PLOT_XLIM)
+    plt.plot(x, perception.b_hist)
+    
+    # 打印模型损失趋势
+    plt.subplot(2, 2, 4, title = "loss")
+    plt.xlim(0, PLOT_XLIM)
+    plt.plot(x, perception.loss_hist)
+
     plt.show()
 
 
